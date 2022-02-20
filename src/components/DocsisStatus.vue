c9f568b52da584c917eee6d1f6482a8172c40cc6
Dobroslav Totev <dev@totev.de>
2022-02-20 21:58:08 +0100

    feat: updated to vue3

---
+++ src/components/DocsisStatus.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import brotliDecompress from "brotli/decompress";
+import { decode } from "@borderless/base64";
+
+import queryString from "query-string";
+// inferred type: { title: string }
+const state = reactive({ docsisJson: "" });
+
+const parsedHash = queryString.parse(location.hash) as any;
+try {
+  const decompressedData = brotliDecompress(decode(parsedHash.docsis));
+  const docsisJson = new TextDecoder().decode(decompressedData);
+  console.log("json:", docsisJson);
+  state.docsisJson = JSON.stringify(JSON.parse(docsisJson), null, 2);
+} catch (error) {
+  console.warn("No hash given", error);
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <span class="green" v-if="state.docsisJson">{{ state.docsisJson }}</span>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    json: {{ json }}
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+    </h3>
   </div>
 </template>
 
-<script>
-import brotliDecompress from "brotli/decompress";
-import { decode } from "@borderless/base64";
-
-import queryString from "query-string";
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      parsedHash: "",
-    };
-  },
-  setup: () => {
-    const parsedHash = queryString.parse(location.hash);
-    let json = "";
-    try {
-      const decompressedData = brotliDecompress(decode(parsedHash.docsis));
-      json = new TextDecoder().decode(decompressedData);
-      console.log("json:", json);
-    } catch (error) {
-      console.warn("No hash given", error);
-    }
-    return { json };
-  },
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
 h3 {
-  margin: 40px 0 0;
+  font-size: 1.2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
 }
 </style>
