d4886dec006a8a822fd66d025825a75dc09b7a1b
Dobroslav Totev <dev@totev.de>
2022-02-28 23:11:43 +0100

    feat: add show/hide toggle to the table viz

--- src/components/DocsisTable.vue
+++ src/components/DocsisTable.vue
@@ -1,46 +1,55 @@
 <template>
   <h2>Table representation</h2>
-  <table class="pure-table-striped">
-    <thead>
-      <tr>
-        <th>Channel ID</th>
-        <th>Channel type</th>
-        <th>Modulation</th>
-        <th>Power level in dBmV</th>
-        <th>Frequency</th>
-        <th>Signal to noise ratio (SNR)</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td colspan="7">Downstream</td>
-      </tr>
-      <tr v-for="downstreamChannel of downstream" :key="downstreamChannel.channelId">
-        <td>{{ downstreamChannel.channelId }}</td>
-        <td>{{ downstreamChannel.channelType }}</td>
-        <td>{{ downstreamChannel.modulation }}</td>
-        <td>{{ downstreamChannel.powerLevel }}</td>
-        <td>{{ getFrequency(downstreamChannel) }}</td>
-        <td>{{ downstreamChannel.snr }}</td>
-      </tr>
-      <tr>
-        <td colspan="7">Upstream</td>
-      </tr>
-      <tr v-for="upstreamChannel of upstream" :key="upstreamChannel.channelId">
-        <td>{{ upstreamChannel.channelId }}</td>
-        <td>{{ upstreamChannel.channelType }}</td>
-        <td>{{ upstreamChannel.modulation }}</td>
-        <td>{{ upstreamChannel.powerLevel }}</td>
-        <td>{{ getFrequency(upstreamChannel) }}</td>
-        <td>{{ upstreamChannel.snr }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <HideShow>
+    <table class="pure-table-striped">
+      <thead>
+        <tr>
+          <th>Channel ID</th>
+          <th>Channel type</th>
+          <th>Modulation</th>
+          <th>Power level in dBmV</th>
+          <th>Frequency</th>
+          <th>Signal to noise ratio (SNR)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td colspan="7">Downstream</td>
+        </tr>
+        <tr
+          v-for="downstreamChannel of downstream"
+          :key="downstreamChannel.channelId"
+        >
+          <td>{{ downstreamChannel.channelId }}</td>
+          <td>{{ downstreamChannel.channelType }}</td>
+          <td>{{ downstreamChannel.modulation }}</td>
+          <td>{{ downstreamChannel.powerLevel }}</td>
+          <td>{{ getFrequency(downstreamChannel) }}</td>
+          <td>{{ downstreamChannel.snr }}</td>
+        </tr>
+        <tr>
+          <td colspan="7">Upstream</td>
+        </tr>
+        <tr
+          v-for="upstreamChannel of upstream"
+          :key="upstreamChannel.channelId"
+        >
+          <td>{{ upstreamChannel.channelId }}</td>
+          <td>{{ upstreamChannel.channelType }}</td>
+          <td>{{ upstreamChannel.modulation }}</td>
+          <td>{{ upstreamChannel.powerLevel }}</td>
+          <td>{{ getFrequency(upstreamChannel) }}</td>
+          <td>{{ upstreamChannel.snr }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </HideShow>
 </template>
 
 <script setup lang="ts">
 import { reactive } from "vue";
 import * as R from "remeda";
+import HideShow from "./HideShow.vue";
 const props = defineProps(["docsisData"]);
 
 const combinedUpstream = R.sortBy(
@@ -70,8 +79,7 @@ function getFrequency(
   if (docsisChannel.frequency) {
     return `${docsisChannel.frequency}`;
   }
-  return `${docsisChannel.frequencyStart}-${docsisChannel.frequencyEnd}`
+  return `${docsisChannel.frequencyStart}-${docsisChannel.frequencyEnd}`;
 }
 </script>
-<style scoped>
-</style>
+<style scoped></style>
