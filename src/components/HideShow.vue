c73fc56b8e9da85414832be0f7357aac229e9b98
Dobroslav Totev <dev@totev.de>
2022-02-28 23:09:38 +0100

    chore: extracted show/hide component

---
+++ src/components/HideShow.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="pure-button button-xsmall" @click="hide = !hide">
+    <i class="fa fa-shopping-cart fa-lg"></i>
+    {{ hide ? "Show" : "Hide" }}
+  </a>
+  <pre :hidden="hide">
+    <slot></slot>
+  </pre>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const hide = ref(true);
+</script>
+<style scoped>
+.button-xsmall {
+  font-size: 70%;
+}
+</style>
